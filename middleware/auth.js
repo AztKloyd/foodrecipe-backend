680831291e6ae8a51912e3f2abcbd03f0b7aed49
AztKloyd <kloyd007@gmail.com>
2025-04-09 19:01:35 +0900

    auth route fix

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,23 +1,21 @@
 const jwt=require("jsonwebtoken")
 
-const verifyToken=async(req,res,next)=>{
-    let token=req.headers["authorization"]
-
-    if(token){
-        token=token.split(" ")[1]
-        jwt.verify(token,process.env.SECRET_KEY,(err,decoded)=>{
-            if(err){
-                return res.status(400).json({message:"Invalid token"})
-            }
-            else{
-                console.log(decoded)
-                req.user=decoded
-            }
-        })
-        next()
+const verifyToken = async (req, res, next) => {
+    let token = req.headers["authorization"];
+  
+    if (token) {
+      token = token.split(" ")[1];
+  
+      jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+        if (err) {
+          return res.status(400).json({ message: "Invalid token" });
+        } else {
+          req.user = decoded;
+          next(); // ✅ 성공 시 여기서만 next 호출
+        }
+      });
+    } else {
+      return res.status(400).json({ message: "Invalid token" });
     }
-    else{
-        return res.status(400).json({message:"Invalid token"})
-    }
-}
+  };
 module.exports=verifyToken
