305bea0e8517a84658154118551bbcdff47cc7e1
AztKloyd <kloyd007@gmail.com>
2025-04-09 15:53:47 +0900

    rollback

--- controller/recipe.js
+++ controller/recipe.js
@@ -24,7 +24,7 @@ const getRecipe=async(req,res)=>{
 }
 
 const addRecipe = async (req, res) => {
-    try {console.log("req.user:", req.user)
+    try {
       const { title, ingredients, instructions, time } = req.body;
   
       if (!title || !ingredients || !instructions) {
